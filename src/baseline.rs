74d8281dea97299b7e846e4e65fc0453f242b09e
Ivan Ukhov <ivan.ukhov@gmail.com>
2018-02-16 11:50:20 +0100

    Complete and test the calculation of the baseline

--- src/baseline.rs
+++ src/baseline.rs
@@ -14,16 +14,22 @@ pub struct Baseline {
 }
 
 struct Mean {
-    sum: Rating,
+    shrinkage: usize,
+    value: Rating,
     count: usize,
 }
 
 impl Baseline {
-    pub fn new<T>(dataset: T) -> Result<Self>
+    pub fn new<T>(
+        dataset: T,
+        n_iterations: usize,
+        user_shrinkage: usize,
+        item_shrinkage: usize,
+    ) -> Result<Self>
     where
         T: Dataset,
     {
-        let mut global_mean = Mean::new();
+        let mut global_mean = Mean::new(0);
         let mut user_means: HashMap<UserID, Mean> = HashMap::new();
         let mut item_means: HashMap<ItemID, Mean> = HashMap::new();
         let mut iterator = dataset.iter_pairs()?;
@@ -31,22 +37,41 @@ impl Baseline {
             global_mean.consume(rating);
             user_means
                 .entry(user_id)
-                .or_insert_with(|| Mean::new())
-                .consume(rating);
+                .or_insert_with(|| Mean::new(user_shrinkage));
             item_means
                 .entry(item_id)
-                .or_insert_with(|| Mean::new())
-                .consume(rating);
+                .or_insert_with(|| Mean::new(item_shrinkage));
+        }
+        global_mean.finalize();
+        for _ in 0..n_iterations {
+            item_means.values_mut().for_each(|mean| mean.reset());
+            let mut iterator = dataset.iter_pairs()?;
+            while let Some(((user_id, item_id), rating)) = iterator.next()? {
+                item_means
+                    .get_mut(&item_id)
+                    .unwrap()
+                    .consume(rating - global_mean.value - user_means[&user_id].value);
+            }
+            item_means.values_mut().for_each(|mean| mean.finalize());
+            user_means.values_mut().for_each(|mean| mean.reset());
+            let mut iterator = dataset.iter_pairs()?;
+            while let Some(((user_id, item_id), rating)) = iterator.next()? {
+                user_means
+                    .get_mut(&user_id)
+                    .unwrap()
+                    .consume(rating - global_mean.value - item_means[&item_id].value);
+            }
+            user_means.values_mut().for_each(|mean| mean.finalize());
         }
         Ok(Baseline {
-            global_bias: global_mean.compute(),
+            global_bias: global_mean.value,
             user_biases: user_means
                 .iter()
-                .map(|(&id, mean)| (id, mean.compute()))
+                .map(|(&id, mean)| (id, mean.value))
                 .collect(),
             item_biases: item_means
                 .iter()
-                .map(|(&id, mean)| (id, mean.compute()))
+                .map(|(&id, mean)| (id, mean.value))
                 .collect(),
         })
     }
@@ -54,18 +79,28 @@ impl Baseline {
 
 impl Mean {
     #[inline]
-    fn new() -> Self {
-        Mean { sum: 0.0, count: 0 }
+    fn new(shrinkage: usize) -> Self {
+        Mean {
+            shrinkage: shrinkage,
+            value: 0.0,
+            count: 0,
+        }
     }
 
     #[inline]
     fn consume(&mut self, value: Rating) {
-        self.sum += value;
+        self.value += value;
         self.count += 1;
     }
 
     #[inline]
-    fn compute(&self) -> Rating {
-        return self.sum / self.count as Rating;
+    fn finalize(&mut self) {
+        self.value /= (self.shrinkage + self.count) as Rating;
+    }
+
+    #[inline]
+    fn reset(&mut self) {
+        self.value = 0.0;
+        self.count = 0;
     }
 }
--- tests/baseline.rs
+++ tests/baseline.rs
@@ -10,10 +10,22 @@ mod reader;
 
 const RATING_PATH: &'static str = "tests/fixtures/ratings.dat";
 
-macro_rules! ok(($result:expr) => ($result.unwrap()));
+macro_rules! assert_equal(
+    ($x:expr, $y:expr) => (assert_eq!($x, $y));
+    ($x:expr, $y:expr, $epsilon:expr) => (assert!(($x - $y).abs() < $epsilon));
+);
+
+macro_rules! ok(
+    ($result:expr) => ($result.unwrap());
+);
 
 #[test]
 fn new() {
     let dataset = Dataset::new(RATING_PATH, Config::new().delimiter("::"));
-    let _ = ok!(Baseline::new(dataset));
+    let baseline = ok!(Baseline::new(dataset, 10, 15, 10));
+    assert_equal!(baseline.global_bias, 3.581564453029317, 1e-10);
+    assert_equal!(baseline.user_biases.len(), 6040);
+    assert_equal!(baseline.item_biases.len(), 3706);
+    assert_equal!(baseline.user_biases[&42], 0.114951013519927, 1e-10);
+    assert_equal!(baseline.item_biases[&42], -0.570961410545486, 1e-10);
 }
